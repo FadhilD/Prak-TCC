25bda538749f540e11faf7651d4ad2c65463999d
FadhilD <mefx226@gmail.com>
2025-05-22 11:32:09 +0700

    tugas7

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,16 @@ import express from "express";
 import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
 
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(NoteRoute);
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
 app.use(NoteRoute);
 
 
